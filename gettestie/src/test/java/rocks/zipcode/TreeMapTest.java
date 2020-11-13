06e930afceb84b905fdc4077915ea03b871e9ca6
Christian Pirhalla <chrispir@udel.edu>
2020-11-13 10:24:14 -0500

    TreeMap added

---
+++ gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
@@ -0,0 +1,61 @@
+package rocks.zipcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapTest {
+    // same as map but maintains ascending order
+    @Test
+    public void testTreeMap1(){
+        TreeMap<Integer, Character> tree = new TreeMap<>();
+
+        //I should be able to add a bunch of Integer keys in a different order and they should
+        // be automatically sorted
+        tree.put(5, 'E');
+        tree.put(1, 'A');
+        tree.put(3, 'C');
+        tree.put(26, 'Z');
+        tree.put(2, 'B');
+        tree.put(4, 'D');
+
+        //Let's see if they got sorted
+        Integer expected = 1;
+        Integer actual = tree.firstKey();
+
+        Integer expected2 = 26;
+        Integer actual2 = tree.lastKey();
+
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+
+        //higherKey?
+        Integer expected3 = 5;
+        Integer actual3 = tree.higherKey(4);
+
+        Assert.assertEquals(expected3, actual3);
+
+        //floorKey
+        Integer expected4 = 3;
+        Integer actual4 = tree.floorKey(4);
+
+
+    }
+
+    @Test
+    public void testTreeMap2(){
+        TreeMap<Integer, Character> tree = new TreeMap<>();
+
+        tree.put(26, 'Z');
+
+        Assert.assertNull(tree.floorKey(26));
+        Assert.assertNull(tree.higherKey(26));
+        Assert.assertNull(tree.remove(1));
+        Assert.assertFalse(tree.isEmpty());
+
+
+    }
+
+}
